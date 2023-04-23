de6f5bcde1292291b488757ead259ba4cc6e5c2d
levanthien1812 <20521947@gm.uit.edu.vn>
2023-04-23 11:47:32 +0700

    add cart popup

--- src/pages/Cart/components/CartPopup.jsx
+++ src/pages/Cart/components/CartPopup.jsx
@@ -1,12 +1,31 @@
-import { Stack, Typography } from '@mui/material'
-import React from 'react'
+import { Stack, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import CartItemCard from "./CartItemCard";
 
 function CartPopup() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    async function fetchCart() {
+      const response = await fetch(
+        "http://localhost:3000/api/carts/?quantity=3",
+        { withCredentials: true, credentials: "include" }
+      );
+
+      const data = await response.json();
+      setItems(data.data);
+    }
+
+    fetchCart();
+  }, []);
+
   return (
-      <Stack p={2}>
-          <Typography>YOUR CART ITEMS</Typography>
+    <Stack p={2}>
+      {items.map((item) => (
+        <CartItemCard item={item} />
+      ))}
     </Stack>
-  )
+  );
 }
 
-export default CartPopup
+export default CartPopup;
