d4c55d5202208326675bfa9b83faf082aca14726
levanthien1812 <20521947@gm.uit.edu.vn>
2023-05-05 11:07:47 +0700

    update profile page and use firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import {
+    initializeApp
+} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCsmVE6uu56AMO2CqW-XBMVYCsTVuxNSt0",
+    authDomain: "sneakerlv.firebaseapp.com",
+    projectId: "sneakerlv",
+    storageBucket: "sneakerlv.appspot.com",
+    messagingSenderId: "396993378300",
+    appId: "1:396993378300:web:e33c9cf74b3a133fc0a479"
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
--- src/pages/Account/Screens/Account.jsx
+++ src/pages/Account/Screens/Account.jsx
@@ -1,22 +1,199 @@
-import { Stack, Typography } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Button,
+  FormControlLabel,
+  FormLabel,
+  Radio,
+  RadioGroup,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Form } from "react-router-dom";
+import { getUser } from "../../../utils/auth";
+import ChangeEmailPopup from "../Components/ChangeEmailPopup";
+import { actions as UIActions } from "../../../store/ui";
+import { useDispatch } from "react-redux";
+import app from '../../../firebase'
 
 function Account() {
+  const user = getUser();
+  const dispatch = useDispatch()
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [phoneNum, setPhoneNum] = useState(user.phoneNum || "Not set");
+  const [birthday, setBirthday] = useState(new Date())
+  const [gender, setGender] = useState(user.gender || 'female')
+  const [photo, setPhoto] = useState(user.photo)
+  const [preview, setPreview] = useState()
+  const [isChangingEmail, setIsChangingEmail] = useState(false)
+
+  const photoChangeHandler = (event) => {
+    setPhoto(event.target.files[0])
+  }
+
+  useEffect(() => {
+    if (typeof(photo) === 'object'){
+      const objectURL = URL.createObjectURL(photo)
+      setPreview(objectURL)
+
+      return () => URL.revokeObjectURL(objectURL);
+    } else {
+      setPreview(photo)
+    }
+  }, [photo])
+
+  const changeEmailClickHandler = () => {
+    setIsChangingEmail(true)
+  }
+
+  const confirmEmailHandler = (newEmail) => {
+    dispatch(
+      UIActions.showNotification({
+        title: "Nofication",
+        message:
+          "We have sent a verification code to your new email! Please copy the code to confirm your email!",
+        type: "success",
+        duration: 4000,
+      })
+    );
+    setTimeout(() => {
+      dispatch(UIActions.hideNotification());
+    }, 4000);
+  }
+
   return (
     <Stack
       flexGrow={1}
       borderRadius={5}
-      padding={3}
+      paddingY={3}
+      paddingX={5}
       sx={{
         backgroundColor: "#fff",
       }}
     >
-      <Typography variant="h2" fontSize={24}>
+      <Typography variant="h2" fontSize={24} marginBottom={3}>
         My profile
-          </Typography>
-          <Stack direction="row">
-              
-          </Stack>
+      </Typography>
+      <Stack direction="row" spacing={3}>
+        <Form style={{ flexGrow: 1, minWidth: "60%" }}>
+          <table cellSpacing={10}>
+            <tbody>
+              <tr>
+                <td>
+                  <FormLabel htmlFor="name">Name: </FormLabel>
+                </td>
+                <td>
+                  <TextField
+                    variant="outlined"
+                    size="small"
+                    id="name"
+                    defaultValue={name}
+                  ></TextField>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <FormLabel htmlFor="email">Email: </FormLabel>
+                </td>
+                <td>
+                  <TextField
+                    variant="outlined"
+                    size="small"
+                    id="email"
+                    defaultValue={email}
+                  ></TextField>
+                  <Button sx={{ textTransform: "capitalize" }} onClick={changeEmailClickHandler}>Change</Button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <FormLabel htmlFor="phoneNum">Phone Number: </FormLabel>
+                </td>
+                <td>
+                  <TextField
+                    variant="outlined"
+                    size="small"
+                    id="phoneNum"
+                    defaultValue={phoneNum}
+                  ></TextField>
+                  <Button sx={{ textTransform: "capitalize" }}>Change</Button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <FormLabel htmlFor="birthday">Birthday: </FormLabel>
+                </td>
+                <td>
+                  <TextField
+                    variant="outlined"
+                    size="small"
+                    id="birthday"
+                    type="date"
+                  ></TextField>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <FormLabel htmlFor="gender">Gender: </FormLabel>
+                </td>
+                <td>
+                  <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue={gender}
+                    name="gender"
+                    row
+                  >
+                    <FormControlLabel
+                      value="female"
+                      control={<Radio />}
+                      label="Female"
+                    />
+                    <FormControlLabel
+                      value="male"
+                      control={<Radio />}
+                      label="Male"
+                    />
+                    <FormControlLabel
+                      value="other"
+                      control={<Radio />}
+                      label="Other"
+                    />
+                  </RadioGroup>
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>
+                  <Button variant="contained">Save</Button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </Form>
+        <Stack alignItems="center" flexGrow={1}>
+          <Box
+            width="160px"
+            height="160px"
+            marginBottom={2}
+            borderRadius="50%"
+            overflow="hidden"
+          >
+            <img src={preview} width="100%" height="100%" />
+          </Box>
+          <TextField
+            type="file"
+            size="small"
+            name="photo"
+            style={{ marginBottom: "12px" }}
+            onChange={photoChangeHandler}
+          />
+          <Typography>Maximum size: 2MB</Typography>
+          <Typography>Accepted Format: JPEG, PNG</Typography>
+        </Stack>
+      </Stack>
+      {isChangingEmail && <ChangeEmailPopup onConfirmEmailHandler={confirmEmailHandler}/>}
     </Stack>
   );
 }
