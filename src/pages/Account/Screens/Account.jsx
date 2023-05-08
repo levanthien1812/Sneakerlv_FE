91d7d3b055398e8aea38008cd2d5bac7ebc40d77
levanthien1812 <20521947@gm.uit.edu.vn>
2023-05-08 22:32:35 +0700

    update user profile (not yet connect to api)

--- src/pages/Account/Screens/Account.jsx
+++ src/pages/Account/Screens/Account.jsx
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Box,
   Button,
   FormControlLabel,
@@ -15,23 +16,38 @@ import { getUser, logout } from "../../../utils/auth";
 import ChangeEmailPopup from "../Components/ChangeEmailPopup";
 import { actions as UIActions } from "../../../store/ui";
 import { useDispatch } from "react-redux";
-import auth from "../../../firebase";
-import {
-  createUserWithEmailAndPassword,
-  sendEmailVerification
-} from "firebase/auth";
+import format from "date-fns/format";
 
 function Account() {
   const user = getUser();
   const dispatch = useDispatch();
-  const [name, setName] = useState(user.name);
+  const [name, setName] = useState(user.name || '');
   const [email, setEmail] = useState(user.email);
   const [phoneNum, setPhoneNum] = useState(user.phoneNum || "Not set");
-  const [birthday, setBirthday] = useState(new Date());
+  const [birthday, setBirthday] = useState(format(new Date(), 'yyyy-MM-dd'));
   const [gender, setGender] = useState(user.gender || "female");
   const [photo, setPhoto] = useState(user.photo);
   const [preview, setPreview] = useState();
   const [isChangingEmail, setIsChangingEmail] = useState(false);
+  const [error, setError] = useState(null)
+
+  const nameChangeHander = (event) => {
+    setName(event.target.value)
+    if (event.target.value.length === 0)
+      return setError("Your name must not be empty!");
+    setError(null)
+  }
+
+  const birthdayChangeHandler = (event) => {
+    if (new Date(event.target.value).getTime() > new Date().getTime())
+      return setError('Your birthday must be before today!')
+    setBirthday(event.target.value)
+    setError(null)
+  }
+
+  const genderChangeHandler = (event) => {
+    setGender(event.target.value)
+  }
 
   const photoChangeHandler = (event) => {
     setPhoto(event.target.files[0]);
@@ -49,7 +65,7 @@ function Account() {
   }, [photo]);
 
   const changeEmailClickHandler = () => {
-    setIsChangingEmail(true);
+    setIsChangingEmail(true)
   };
 
   const confirmEmailHandler = (newEmail) => {
@@ -67,6 +83,35 @@ function Account() {
     }, 4000);
   };
 
+  const updateProfileHandler = async (event) => {
+    event.preventDefault()
+    try {
+      const userFormdata = new FormData();
+      userFormdata.append("name", name)
+      userFormdata.append("email", email);
+      userFormdata.append("phoneNum", phoneNum);
+      userFormdata.append("birthday", birthday);
+      userFormdata.append("gender", gender);
+      userFormdata.append("photo", photo);
+
+      const response = await fetch("http://localhost:3000/api/users/account/profile", {
+        method: "patch",
+        body: userFormdata
+      })
+
+      if (!response.ok) {
+        return 
+      }
+
+      const data = await response.json();
+
+      if (data.status === "fail") { }
+    }
+    catch (err) {
+
+    }
+  }
+
   return (
     <Stack
       flexGrow={1}
@@ -80,131 +125,146 @@ function Account() {
       <Typography variant="h2" fontSize={24} marginBottom={3}>
         My profile
       </Typography>
-      <Stack direction="row" spacing={3}>
-        <Form style={{ flexGrow: 1, minWidth: "60%" }}>
-          <table cellSpacing={10}>
-            <tbody>
-              <tr>
-                <td>
-                  <FormLabel htmlFor="name">Name: </FormLabel>
-                </td>
-                <td>
-                  <TextField
-                    variant="outlined"
-                    size="small"
-                    id="name"
-                    defaultValue={name}
-                  ></TextField>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <FormLabel htmlFor="email">Email: </FormLabel>
-                </td>
-                <td>
-                  <TextField
-                    variant="outlined"
-                    size="small"
-                    id="email"
-                    defaultValue={email}
-                  ></TextField>
-                  <Button
-                    sx={{ textTransform: "capitalize" }}
-                    onClick={changeEmailClickHandler}
-                  >
-                    Change
-                  </Button>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <FormLabel htmlFor="phoneNum">Phone Number: </FormLabel>
-                </td>
-                <td>
-                  <TextField
-                    variant="outlined"
-                    size="small"
-                    id="phoneNum"
-                    defaultValue={phoneNum}
-                  ></TextField>
-                  <Button sx={{ textTransform: "capitalize" }}>Change</Button>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <FormLabel htmlFor="birthday">Birthday: </FormLabel>
-                </td>
-                <td>
-                  <TextField
-                    variant="outlined"
-                    size="small"
-                    id="birthday"
-                    type="date"
-                  ></TextField>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <FormLabel htmlFor="gender">Gender: </FormLabel>
-                </td>
-                <td>
-                  <RadioGroup
-                    aria-labelledby="demo-radio-buttons-group-label"
-                    defaultValue={gender}
-                    name="gender"
-                    row
-                  >
-                    <FormControlLabel
-                      value="female"
-                      control={<Radio />}
-                      label="Female"
-                    />
-                    <FormControlLabel
-                      value="male"
-                      control={<Radio />}
-                      label="Male"
-                    />
-                    <FormControlLabel
-                      value="other"
-                      control={<Radio />}
-                      label="Other"
-                    />
-                  </RadioGroup>
-                </td>
-              </tr>
-              <tr>
-                <td></td>
-                <td>
-                  <Button variant="contained">Save</Button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </Form>
-        <Stack alignItems="center" flexGrow={1}>
-          <Box
-            width="160px"
-            height="160px"
-            marginBottom={2}
-            borderRadius="50%"
-            overflow="hidden"
-          >
-            <img src={preview} width="100%" height="100%" />
-          </Box>
-          <TextField
-            type="file"
-            size="small"
-            name="photo"
-            style={{ marginBottom: "12px" }}
-            onChange={photoChangeHandler}
-          />
-          <Typography>Maximum size: 2MB</Typography>
-          <Typography>Accepted Format: JPEG, PNG</Typography>
+      <Stack>
+        {error && <Alert severity="error">{error}</Alert>}
+        <Stack direction="row" spacing={3} marginTop={1}>
+          <Form style={{ flexGrow: 1, minWidth: "60%" }}>
+            <table cellSpacing={10}>
+              <tbody>
+                <tr>
+                  <td>
+                    <FormLabel htmlFor="name">Name: </FormLabel>
+                  </td>
+                  <td>
+                    <TextField
+                      variant="outlined"
+                      size="small"
+                      id="name"
+                      value={name}
+                      onChange={nameChangeHander}
+                    ></TextField>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <FormLabel htmlFor="email">Email: </FormLabel>
+                  </td>
+                  <td>
+                    <TextField
+                      variant="outlined"
+                      size="small"
+                      id="email"
+                      defaultValue={email}
+                      InputProps={{
+                        readOnly: true,
+                      }}
+                    ></TextField>
+                    <Button
+                      sx={{ textTransform: "capitalize" }}
+                      onClick={changeEmailClickHandler}
+                    >
+                      Change
+                    </Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <FormLabel htmlFor="phoneNum">Phone Number: </FormLabel>
+                  </td>
+                  <td>
+                    <TextField
+                      variant="outlined"
+                      size="small"
+                      id="phoneNum"
+                      defaultValue={phoneNum}
+                      InputProps={{
+                        readOnly: true,
+                      }}
+                    ></TextField>
+                    <Button sx={{ textTransform: "capitalize" }}>Change</Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <FormLabel htmlFor="birthday">Birthday: </FormLabel>
+                  </td>
+                  <td>
+                    <TextField
+                      variant="outlined"
+                      size="small"
+                      id="birthday"
+                      type="date"
+                      value={birthday}
+                      onChange={birthdayChangeHandler}
+                    ></TextField>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <FormLabel htmlFor="gender">Gender: </FormLabel>
+                  </td>
+                  <td>
+                    <RadioGroup
+                      aria-labelledby="demo-radio-buttons-group-label"
+                      name="gender"
+                      row
+                      value={gender}
+                      onChange={genderChangeHandler}
+                    >
+                      <FormControlLabel
+                        value="female"
+                        control={<Radio />}
+                        label="Female"
+                      />
+                      <FormControlLabel
+                        value="male"
+                        control={<Radio />}
+                        label="Male"
+                      />
+                      <FormControlLabel
+                        value="other"
+                        control={<Radio />}
+                        label="Other"
+                      />
+                    </RadioGroup>
+                  </td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>
+                    <Button variant="contained" onClick={updateProfileHandler} disabled={error !== null}>
+                      Save
+                    </Button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </Form>
+          <Stack alignItems="center" flexGrow={1}>
+            <Box
+              width="200px"
+              height="200px"
+              marginBottom={2}
+              borderRadius="50%"
+              overflow="hidden"
+            >
+              <img src={preview} width="100%" height="100%" />
+            </Box>
+            <TextField
+              type="file"
+              size="small"
+              name="photo"
+              style={{ marginBottom: "12px" }}
+              onChange={photoChangeHandler}
+            />
+            <Typography>Maximum size: 2MB</Typography>
+            <Typography>Accepted Format: JPEG, PNG</Typography>
+          </Stack>
         </Stack>
+        {isChangingEmail && (
+          <ChangeEmailPopup onConfirmEmailHandler={confirmEmailHandler} />
+        )}
       </Stack>
-      {isChangingEmail && (
-        <ChangeEmailPopup onConfirmEmailHandler={confirmEmailHandler} />
-      )}
     </Stack>
   );
 }
